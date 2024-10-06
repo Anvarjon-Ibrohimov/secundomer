cfee72c4d8270a9efdbe580f9a337c6c91a670bb
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-10-06 10:13:10 +0500

    Secundomer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { hover } from "@testing-library/user-event/dist/hover";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secund: 0,
+      minute: 0,
+      hour: 0,
+      disabled: false,
+      interval: "",
+      intervalStorage: [],
+    };
+  }
+  start = () => {
+    this.setState({
+      disabled: true,
+    });
+    let time = setInterval(() => {
+      if (this.state.minute === 59) {
+        this.setState({
+          secund: 0,
+          minute: 0,
+          hour: this.state.hour + 1,
+        });
+      } else {
+        if (this.state.secund === 59) {
+          this.setState({
+            secund: 0,
+            minute: this.state.minute + 1,
+          });
+        } else {
+          this.setState({
+            secund: this.state.secund + 1,
+          });
+        }
+      }
+    }, 10);
+    this.setState({
+      interval: time,
+    });
+  };
+  stop = () => {
+    clearInterval(this.state.interval);
+    this.setState({
+      disabled: false,
+    });
+  };
+  clear = () => {
+    this.setState({
+      secund: 0,
+      minute: 0,
+      hour: 0,
+      intervalStorage: [],
+    });
+  };
+  interval = () => {
+    this.state.intervalStorage.push(
+      `${this.state.hour}:${this.state.minute}:${this.state.secund}`
+    );
+    this.setState({
+      intervalStorage: this.state.intervalStorage,
+    });
+  };
+  render() {
+    const { secund, minute, hour, disabled, intervalStorage } = this.state;
+    return (
+      <div className="App">
+        <div className="heading">
+          <h2>Secundomer</h2>
+        </div>
+        <div className="timer">
+          <p className="h4">
+            {hour}:{minute}:{secund}
+          </p>
+        </div>
+        <div className="btn btn-group">
+          <button
+            className="btn btn-success"
+            onClick={this.start}
+            disabled={disabled}>
+            Start
+          </button>
+          <button className="btn btn-warning" onClick={this.stop}>
+            Stop
+          </button>
+          <button className="btn btn-danger" onClick={this.clear}>
+            Clear
+          </button>
+          <button className="btn btn-dark" onClick={this.interval}>
+            Interval
+          </button>
+        </div>
+        <div className="interval-storage">
+          <hr />
+          <h3>Intervals</h3>
+          {intervalStorage.map((datatime, index) => {
+            return (
+              <h5 className="">
+                {index + 1} - {datatime}
+              </h5>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
